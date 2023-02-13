0b2d0b485ee84d7bd2e13d00d66dcd101e95b4e9
Rohit-Kiran <111106664+Rohit-Kiran@users.noreply.github.com>
2023-02-13 21:27:06 +0530

    Update RF HeartDisease-Copy2.py

--- RF HeartDisease-Copy2.py
+++ RF HeartDisease-Copy2.py
@@ -487,10 +487,98 @@ joblib.dump(rf,'model_joblib_heart')
 model=joblib.load('model_joblib_heart')
 
 
-# In[80]:
+# In[74]:
 
 
 model.predict(new_data)
+
+
+# In[75]:
+
+
+from tkinter import *
+import joblib
+def show_entry_fields():
+    p1=int(e1.get())
+    p2=int(e2.get())
+    p3=int(e3.get())
+    p4=int(e4.get())
+    p5=int(e5.get())
+    p6=int(e6.get())
+    p7=int(e7.get())
+    p8=int(e8.get())
+    p9=int(e9.get())
+    p10=float(e10.get())
+    p11=int(e11.get())
+    p12=int(e12.get())
+    p13=int(e13.get())
+    model = joblib.load('model_joblib_heart')
+    result=model.predict([[p1,p2,p3,p4,p5,p6,p7,p8,p8,p10,p11,p12,p13]])
+    
+    if result == 0:
+        Label(master, text="No Possibility of Heart Disease",bg = "white", fg = "dark green").grid(row=31)
+    else:
+        Label(master, text=" Possibility of Heart Disease ",bg = "white", fg = "red").grid(row=31)
+    
+    
+master = Tk()
+master.title("Heart Disease Prediction System")
+
+
+label = Label(master, text = "Heart Disease Prediction System"
+                          , bg = "black", fg = "white",width=100). \
+                               grid(row=0,columnspan=2)
+
+
+Label(master, text="Enter Your Age").grid(row=1)
+Label(master, text="Male Or Female [1/0]").grid(row=2)
+Label(master, text="ChestPain (0-3)").grid(row=3)
+Label(master, text="Rest BPS").grid(row=4)
+Label(master, text="Cholestrol").grid(row=5)
+Label(master, text="Fasting Blood Sugar (in mg/l)").grid(row=6)
+Label(master, text="RestECG").grid(row=7)
+Label(master, text="Maximum Heart Rate").grid(row=8)
+Label(master, text="Exercise Induced Angina").grid(row=9)
+Label(master, text="OldPeek").grid(row=10)
+Label(master, text="Hear Rate Slope").grid(row=11)
+Label(master, text="Major Vessels (0-3)").grid(row=12)
+Label(master, text="Thalassemia").grid(row=13)
+
+
+
+e1 = Entry(master)
+e2 = Entry(master)
+e3 = Entry(master)
+e4 = Entry(master)
+e5 = Entry(master)
+e6 = Entry(master)
+e7 = Entry(master)
+e8 = Entry(master)
+e9 = Entry(master)
+e10 = Entry(master)
+e11 = Entry(master)
+e12 = Entry(master)
+e13 = Entry(master)
+
+e1.grid(row=1, column=1)
+e2.grid(row=2, column=1)
+e3.grid(row=3, column=1)
+e4.grid(row=4, column=1)
+e5.grid(row=5, column=1)
+e6.grid(row=6, column=1)
+e7.grid(row=7, column=1)
+e8.grid(row=8, column=1)
+e9.grid(row=9, column=1)
+e10.grid(row=10, column=1)
+e11.grid(row=11, column=1)
+e12.grid(row=12, column=1)
+e13.grid(row=13, column=1)
+
+
+
+Button(master, text='Predict',bg = "cyan", fg = "black", command=show_entry_fields).grid()
+
+mainloop()
 
 
 # In[ ]:
